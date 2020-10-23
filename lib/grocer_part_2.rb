b80e1346c8eb48a83ad690dbe0f682187b422931
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-10-23 16:24:54 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,7 +1,12 @@
 require_relative './part_1_solution.rb'
 require 'pry'
 def apply_coupons(cart, coupons)
-  cart.each do |key|
+  i = 0
+  coupons.each do |key|
+  discount_item = find_item_by_name_in_collection(coupons[:item], cart)
+  in_cart = !!discount_item
+  valid_coupon = in_cart && discount_item[:num] 
+
     binding.pry
   end
 end
