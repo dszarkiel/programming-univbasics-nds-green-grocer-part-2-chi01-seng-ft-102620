ac9882f64fb45ff2075d44c89d930c7117d08eae
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-10-23 16:40:25 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -7,7 +7,7 @@ def apply_coupons(cart, coupons)
   in_cart = !!discount_item
   valid_coupon = in_cart && discount_item[:count] >= coupon[:num]
   if discount_item && valid_coupon
-    cart << { item: "#{discount_item[:item]} W/ COUPON",
+    cart << { item: "#{discount_item[:item]} W/COUPON",
     price:  coupon[:cost]/coupon[:num],
     clearance: discount_item[:clearance],
     count: coupon[:num]
