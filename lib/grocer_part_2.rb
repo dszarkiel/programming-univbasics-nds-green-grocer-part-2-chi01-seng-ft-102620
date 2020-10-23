42a6b2a3ac90358330750e59301986d9305a2239
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-10-23 16:39:08 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -2,10 +2,10 @@ require_relative './part_1_solution.rb'
 require 'pry'
 def apply_coupons(cart, coupons)
   i = 0
-  coupons.each do |key|
+  coupons.each do |coupon|
   discount_item = find_item_by_name_in_collection(coupons[:item], cart)
   in_cart = !!discount_item
-  valid_coupon = in_cart && coupons[:num] <= discount_item[:count]
+  valid_coupon = in_cart && discount_item[:count] >= coupon[:num]
   if discount_item && valid_coupon
     cart << { item: "#{coupons[:item]} W/ COUPON",
     price:  coupon[:cost]/coupon[:num],
